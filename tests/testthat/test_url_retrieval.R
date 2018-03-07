0fb3d23501e2564e172467b42c579e6c67c95fc8
HorridTom <woodcock.thomas@gmail.com>
2018-03-07 17:29:19 +0000

    Test for url structure

--- tests/testthat/test_url_retrieval.R
+++ tests/testthat/test_url_retrieval.R
@@ -1,7 +1,28 @@
 library(nhsAEscraper)
 context("Retrieve urls of A&E data files")
 
+library(stringr)
+library(zoo)
+
 test_that("getAEdata_urls_monthly returns character vector",{
   urls_ae <- getAEdata_urls_monthly()
   expect_is(urls_ae, "character")
+})
+
+test_that("getAEdata_urls_monthly returns urls with correct anatomy",{
+  urls_ae <- getAEdata_urls_monthly()
+
+  url_pattern <- '^https://www.england.nhs.uk/statistics/wp-content/uploads/sites/.*$'
+  filename_pattern <- '/(?:.(?!/))+(-AE-by-provider-).+xls.?$'
+  date_pattern <- '^(.*)-AE-by-provider.*$'
+
+  url_stem_presence <- str_detect(urls_ae, url_pattern)
+  filename_strings <- str_match(urls_ae, filename_pattern)[,1]
+  filename_strings <- str_match(filename_strings, '^/(.*)$')[,2]
+  dates <- str_match(filename_strings, date_pattern)[,2]
+  dates_valid <- !is.na(as.yearmon(dates, "%B-%Y"))
+
+  expect_equal(all(url_stem_presence), TRUE, info = 'Url stem')
+  expect_equal(all(!is.na(filename_strings)), TRUE, info = 'Filename components')
+  expect_equal(all(dates_valid), TRUE, info = 'Month-Year')
 })
