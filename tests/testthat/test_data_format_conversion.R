925a672da17ff54f0456ea69e9e6e58011abc1df
HorridTom <woodcock.thomas@gmail.com>
2018-03-13 10:55:52 +0000

    Added test for format checking.
    Moved data load to pre-test fixture.

--- tests/testthat/test_data_format_conversion.R
+++ tests/testthat/test_data_format_conversion.R
@@ -1,9 +1,10 @@
 library(nhsAEscraper)
 context("Convert imported data to a standard format")
 
+rawDataList <- load_AE_files(directory = 'test-data/')
+rawDataList_conv <- lapply(rawDataList, delete_extra_columns)
+
 test_that("Superfluous columns correctly deleted",{
-  rawDataList <- load_AE_files(directory = 'test-data/')
-  rawDataList_conv <- lapply(rawDataList, delete_extra_columns)
 
   comparison_1 <- compare::compare(rawDataList[[1]], rawDataList_conv[[1]])
   comparison_2 <- compare::compare(rawDataList[[2]], rawDataList_conv[[2]])
@@ -16,4 +17,10 @@ test_that("Superfluous columns correctly deleted",{
   expect_match(rawDataList_conv[[1]][[21]], "Number of patients spending >12 hours", all = FALSE, info = 'correct last column heading present (still correct)')
   expect_match(rawDataList_conv[[2]][[21]], "Number of patients spending >12 hours", all = FALSE, info = 'correct last column heading present (after change)')
 
+})
+
+test_that("Pick up incorrect formats",{
+  expect_equal(check_format(rawDataList[[1]]), TRUE, info = 'valid format detected correctly')
+  expect_equal(check_format(rawDataList[[2]]), FALSE, info = 'invalid format detected correctly')
+  expect_equal(check_format(rawDataList_conv[[2]]), TRUE, info = 'valid format (post-column-correction) detected correctly')
 })
